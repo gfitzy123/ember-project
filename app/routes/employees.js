ede4e37b477d22130197cf89d244db0dd79082db
Garrett <gfitzgerald123@gmail.com>
2015-07-28 22:05:33 -0500

    initializer undefined?

--- app/routes/employees.js
+++ app/routes/employees.js
@@ -1,7 +1,22 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+		employeeName: null,
+		employeeSalary: null,
 	model: function() {
 		return this.store.find('employee');
-	}
+
+	},
+	actions: {
+		addEmployee: function() {
+		
+			console.log(this.get('employeeName'));
+				var employee = this.store.createRecord('employee', {
+					name: this.get('employeeName'),
+					salary: this.get('employeeSalary')
+				});
+
+				employee.save();
+			}
+		}
 });
